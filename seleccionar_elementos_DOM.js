09d67f96f430380d63cabc9bf7a9e26bb8ac2e89
Juan <jmoralespuello@gmail.com>
2018-11-29 18:13:28 -0500

    Seleccionar elementos DOM 3

--- seleccionar_elementos_DOM.js
+++ seleccionar_elementos_DOM.js
@@ -1,6 +1,24 @@
-/* var div = document.getElementById("mi_div");
-div.classList.add("my_class"); */
+var div = document.getElementById("mi_div");
+//div.classList.add("my_class");
 
-var div = document.getElementsByClassName("my_class");
+var div1 = document.getElementsByClassName("my_class");
+console.log(div1);
 
-console.log(div);
+var div2 = document.getElementsByTagName("div");
+console.log(div2);
+
+var mi_clase = document.querySelector(".my_class");
+console.log(mi_clase);
+
+var mi_clase2 = document.querySelectorAll(".my_class");
+console.log(mi_clase2);
+
+var div3 = document.querySelectorAll("div");
+console.log(div3);
+
+function $(selector){
+	return document.querySelector(selector);
+}
+
+var mi_clase3 = $(".my_class");
+console.log(mi_clase3);
