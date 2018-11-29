9c1f99e63a556d7f196e93cf526707bf2d6c96b9
Juan <jmoralespuello@gmail.com>
2018-11-29 18:52:32 -0500

    Eventos

---
+++ eventos.js
@@ -0,0 +1,12 @@
+document.getElementById("divclick").addEventListener("click",clic2,false);
+document.getElementById("btnclick").addEventListener("click",clic,true);
+
+document.getElementById("btnMouseOver").addEventListener("mouseover",clic,false);
+
+function clic(){
+  alert("Hooolaaaaaaaaaaaaaaaaaa");
+}
+
+function clic2(){
+  alert("Adiooooooosss");
+}
