6fe85422efa34acb9d12cf5c994bedd7bef92e37
Juan <jmoralespuello@gmail.com>
2018-12-20 16:40:32 -0500

    Pong: Finalizado

--- pong/main.js
+++ pong/main.js
@@ -48,17 +48,38 @@
     this.y = y;
     this.radius = radius;
     this.board = board;
+    this.speed = 3;
     this.speed_y = 0;
     this.speed_x = 3;
-    this.direccion = 1;
+    this.direction = 1;
+    this.bounce_angle = 0;
+    this.max_bounce_angle = Math.PI/12;
     this.board.ball = this;
     this.kind = "circle";
   }
 
   self.Ball.prototype = {
     move: function(){
-      this.x += (this.speed_x * this.direccion);
+      this.x += (this.speed_x * this.direction);
       this.y += (this.speed_y);
+    },
+    get width(){
+      return this.radius * 2;
+    },
+    get height(){
+      return this.radius * 2;
+    },
+    collision: function(bar){
+      // Reacciona a la colisión con una barra que recibe como parámetro
+      var relative_intersect_y = (bar.y+(bar.height/2)) - this.y;
+      var normalized_intersect_y = relative_intersect_y / (bar.height/2);
+      
+      this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+      this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+      this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+      if(this.x > (this.board.width/2)) this.direction = -1;
+      else this.direction = 1;
     }
   }
 })();
@@ -81,29 +102,57 @@
         draw(this.ctx,this.board.elements[i]);
       }
     },
+    check_collisions: function(){
+      for (var i = this.board.bars.length - 1; i >= 0; i--) {
+        var bar = this.board.bars[i];
+        if(hit(bar,this.board.ball)){
+          this.board.ball.collision(bar);
+        }
+      }
+    },
     play: function(){
       if(this.board.playing){
         this.clean();
         this.draw();
+        this.check_collisions();
         this.board.ball.move();
       }
     }
   }
 
+  function hit(a,b) {
+    // Revisa si a colisiona con b
+    var hit=false;
+    // Colisiones horizontales
+    if(b.x + b.width >= a.x && b.x < a.x + a.width){
+      // Colisiones verticales
+      if(b.y + b.height >= a.y && b.y < a.y + a.height) hit=true;
+    }
+    // Colision de a con b
+    if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+      if(b.y <= a.y && b.y + b.height >= a.y + a.height) hit=true;
+    }
+    // Colision de b con a
+    if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+      if(a.y <= b.y && a.y + a.height >= b.y + b.height) hit=true;
+    }
+    return hit;
+  }
+
   function draw(ctx,element){
     switch(element.kind){
-      case "rectangle":
+      case "rectangle": {
         ctx.fillRect(element.x,element.y,element.width,element.height);
         break;
-
-      case "circle":
+      }
+      case "circle": {
         ctx.beginPath();
         ctx.arc(element.x,element.y,element.radius,0,7);
         ctx.fill();
         ctx.closePath();
         break;
-
-      default: break;
+      }
+      default: { break;}
     }
   }
 })();
@@ -116,26 +165,24 @@ var board_view = new BoardView(canvas,board);
 var ball = new Ball(350,100,10,board);
 
 document.addEventListener("keydown",function(ev){
-  ev.preventDefault();
   if(ev.keyCode==38){
+    ev.preventDefault();
     bar2.up();
   }else if(ev.keyCode==40){
     bar2.down();
   }else if(ev.keyCode==87){
     bar.up();
   }else if(ev.keyCode==83){
+    ev.preventDefault();
     bar.down();
   }else if(ev.keyCode==32){
     board.playing = !board.playing;
   }
-  
 });
 
-//window.addEventListener("load",main);
 window.requestAnimationFrame(controller);
 
 function controller() {
-  //console.log("Controller");
   board_view.play();
   window.requestAnimationFrame(controller);
 }
