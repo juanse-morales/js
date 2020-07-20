7d8f68b2706edafd572753d8391043d83dcff4b0
Juan <jmoralespuello@gmail.com>
2020-07-20 13:49:28 -0500

    Add folder: curso_udemy/condicional_switch

---
+++ curso_udemy/condicional_switch/switch.js
@@ -0,0 +1,29 @@
+// Switch
+
+var edad = 18;
+var imprime = "";
+
+switch(edad){
+  case 18:{
+    imprime = "Acabas de ser mayor de edad";
+    break;
+  }
+  case 25:{
+    imprime = "Ya eres un adulto";
+    break;
+  }
+  case 40:{
+    imprime = "Crisis de los 40";
+    break;
+  }
+  case 75:{
+    imprime = "Ya eres un anciano";
+    break;
+  }
+  default:{
+    imprime = "Edad neutra";
+    break;
+  }
+}
+
+console.log(imprime);
