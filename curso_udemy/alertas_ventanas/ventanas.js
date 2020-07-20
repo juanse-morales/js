1a4716a606ed4c11c1120c08419ae156eba97dcb
Juan <jmoralespuello@gmail.com>
2020-07-20 16:18:12 -0500

    Add folder: curso_udemy/alertas_ventanas

---
+++ curso_udemy/alertas_ventanas/ventanas.js
@@ -0,0 +1,12 @@
+// Alertas y Ventanas
+
+// Alerta
+alert('Esta es mi alerta!');
+
+// Confirmación
+var respuesta = confirm('¿Desea continuar?');
+console.log(respuesta);
+
+// Ingreso de datos
+var edad = prompt('Qué edad tienes?',18);
+console.log(edad);
