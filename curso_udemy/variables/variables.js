b3e53ff6c9384a7a06ee039ca7d755f440c15b15
Juan <jmoralespuello@gmail.com>
2020-07-19 21:03:51 -0500

    Add folder: curso_udemy/variables

---
+++ curso_udemy/variables/variables.js
@@ -0,0 +1,12 @@
+// Variable: es un contenedor de información
+
+var pais = "España";
+var continente = "Europa";
+var antiguedad = 2019;
+var pais_continente = pais+' '+continente;
+
+pais = 'Mexico';
+continente = 'Latinoamerica';
+
+console.log(pais, continente, antiguedad);
+console.log(pais_continente);
