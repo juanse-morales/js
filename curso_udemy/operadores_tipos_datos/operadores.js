504e3f7f3c05eebac9b7761c7fe811071cd627b5
Juan <jmoralespuello@gmail.com>
2020-07-20 12:49:29 -0500

    Add folder: curso_udemy/operadores_tipos_datos

---
+++ curso_udemy/operadores_tipos_datos/operadores.js
@@ -0,0 +1,24 @@
+// Operadores y tipos de datos
+
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert("El resultado de la operación es: "+operacion);
+
+// Tipos de datos
+
+var numero = "33";
+
+console.log(numero+7);
+console.log(Number(numero)+7);
+console.log(parseInt(numero)+7);
+console.log(parseFloat(numero)+7);
+
+console.log(numero1+' Texto');
+console.log(String(numero1)+7);
+
+// Typeof
+
+console.log(typeof numero1);
+console.log(typeof numero);
