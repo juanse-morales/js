8d7dedbffa14053e096828bf1754173ae50e0089
Juan <jmoralespuello@gmail.com>
2020-08-24 17:56:59 -0500

    New folder: curso_udemy_fh

---
+++ curso_udemy_fh/01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,9 @@
+/* alert('Hola Mundo');
+
+let nombre = prompt('¿Cuál es tu nombre?');
+console.log(nombre);
+
+const seleccion = confirm('¿Estás seguro de borrar?');
+console.log(seleccion);
+*/
+console.log(global);
---
+++ curso_udemy_fh/01-fundamentos/assets/js/app.js
@@ -0,0 +1,28 @@
+// alert('Hola Mundo desde app.js');
+
+// console.log('Hola Mundo');
+
+let a = 10,
+  b = 20,
+  c = 'Hola ',
+  d = 'Spiderman',
+  x = a + b;
+
+const saludo = c + d;
+/*
+console.log(x);
+console.warn(a);
+console.error(b);
+console.info(c);
+
+console.log('%c Mis variables','color:blue; font-weight: bold;');
+console.log({a});
+console.log({b});
+console.log({c});
+
+console.log({a,b,c,d,x});
+console.table({a,b,c,d,x});
+*/
+c = 'Hola de nuevo';
+
+let miNombre = 'Juan';
